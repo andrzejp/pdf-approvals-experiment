db7f004b138d186238a602aa4a2231a0b8c0d37d
Andrzej Prochyra <andrzej@prochyra.name>
2023-11-18 17:47:43 +0000

    Initial commit

---
+++ src/test/java/io/prochyra/PdfApprovalsTest.java
@@ -0,0 +1,13 @@
+package io.prochyra;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PdfApprovalsTest {
+
+    @Test
+    void test_nothing() {
+        assertThat(true).isTrue();
+    }
+}
